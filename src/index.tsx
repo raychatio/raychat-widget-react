a22c3b939a8c0789a696eb385d19f31ac98723cd
Mahdi Vajdi <mahdivajdii@gmail.com>
2024-01-07 19:13:15 +0330

    add types to the project

--- src/index.tsx
+++ src/index.tsx
@@ -3,6 +3,8 @@ import React, { useEffect } from "react";
 declare global {
   interface Window {
     RAYCHAT_TOKEN: string;
+    SEO_FRIENDLY: boolean;
+    FAST_LOAD: boolean;
   }
 }
 
@@ -11,31 +13,34 @@ interface RaychatProps {
   type?: string;
 }
 
-const Raychat: React.FC<RaychatProps> = ({ token, type = "normal" }) => {
+const RaychatWidget: React.FC<RaychatProps> = ({ token, type = "normal" }) => {
   useEffect(() => {
     if (!token || token.length === 0) return undefined;
 
-    if (typeof window !== "undefined") window.RAYCHAT_TOKEN = token;
+    if (typeof window !== "undefined") {
+      window.RAYCHAT_TOKEN = token;
 
-    if (type === "normal") {
-      const script = document.createElement("script");
-      script.type = "text/javascript";
-      script.async = true;
-      script.src = "https://widget-react.raychat.io/install/widget.js";
-
-      document.head.appendChild(script);
-
-      // Cleanup: remove the script element when the component is unmounted
-      return () => {
-        document.head.removeChild(script);
-      };
+      if (type === "SEO_FRIENDLY") {
+        window.SEO_FRIENDLY = true;
+      } else if (type === "FAST_LOAD") {
+        window.FAST_LOAD = true;
+      }
     }
 
-    // Explicitly return undefined when type is not "normal"
-    return undefined;
+    const script = document.createElement("script");
+    script.type = "text/javascript";
+    script.async = true;
+    script.src = "https://widget-react.raychat.io/install/widget.js";
+
+    document.head.appendChild(script);
+
+    // Cleanup: remove the script element when the component is unmounted
+    return () => {
+      document.head.removeChild(script);
+    };
   }, [token, type]);
 
   return null; // Raychat doesn't render any additional content
 };
 
-export default Raychat;
+export default RaychatWidget;
