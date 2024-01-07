4fb7936bfbf2f7d8a6b97a3903e3b44e4b4ff27c
Mahdi Vajdi <mahdivajdii@gmail.com>
2024-01-07 21:34:33 +0330

    refactor: add check for type prop

--- src/index.tsx
+++ src/index.tsx
@@ -13,16 +13,28 @@ interface RaychatProps {
   type?: string;
 }
 
-const RaychatWidget: React.FC<RaychatProps> = ({ token, type = "normal" }) => {
+const RaychatWidget: React.FC<RaychatProps> = ({ token, type = "NORMAL" }) => {
   useEffect(() => {
-    if (!token || token.length === 0) return undefined;
+    // Validate token
+    if (!token || token.trim() === "") {
+      console.error('Token for raychat widget is not provided.');
+      return undefined;
+    }
+
+    // Validate type
+    const validTypes = ['NORMAL', 'SEO_FRIENDLY', 'FAST_LOAD'];
+    if (type && !validTypes.includes(type.toUpperCase())) {
+      console.warn(
+        `Provided type for raychat widget: "${type}" is not valid. using type "NORMAL" instead.`
+      );
+    }
 
     if (typeof window !== "undefined") {
       window.RAYCHAT_TOKEN = token;
 
-      if (type === "SEO_FRIENDLY") {
+      if (type.toUpperCase() === "SEO_FRIENDLY") {
         window.SEO_FRIENDLY = true;
-      } else if (type === "FAST_LOAD") {
+      } else if (type.toUpperCase() === "FAST_LOAD") {
         window.FAST_LOAD = true;
       }
     }
@@ -31,7 +43,6 @@ const RaychatWidget: React.FC<RaychatProps> = ({ token, type = "normal" }) => {
     script.type = "text/javascript";
     script.async = true;
     script.src = "https://widget-react.raychat.io/install/widget.js";
-
     document.head.appendChild(script);
 
     // Cleanup: remove the script element when the component is unmounted
